27cb2cdf4218021901d4e29dea0f6595da73c0fe
sarahmarshy <sarahmarshy@localhost>
2015-06-15 16:29:24 +0000

    Working example of RPC over HTTP.

--- HTTP_RPC.cpp
+++ HTTP_RPC.cpp
@@ -20,51 +20,41 @@ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 THE SOFTWARE.
 */
 #include "mbed.h"
-#include "EthernetNetIf.h"
+#include "EthernetInterface.h"
 #include "HTTPServer.h"
+#include "mbed_rpc.h"
 
-DigitalOut led1(LED1, "led1");
-DigitalOut led2(LED2, "led2");
-DigitalOut led3(LED3, "led3");
-DigitalOut led4(LED4, "led4");
+DigitalOut led1(LED1);
+RpcDigitalOut led2(LED2,"led2");
+RpcDigitalOut led3(LED3,"led3");
+RpcDigitalOut led4(LED4, "led4");
 
-LocalFileSystem fs("webfs");
 
-EthernetNetIf eth;  
+EthernetInterface eth;  
 HTTPServer svr;
 
 int main() {
-    Base::add_rpc_class<AnalogIn>();
-    Base::add_rpc_class<AnalogOut>();
-    Base::add_rpc_class<DigitalIn>();
-    Base::add_rpc_class<DigitalOut>();
-    Base::add_rpc_class<DigitalInOut>();
-    Base::add_rpc_class<PwmOut>();
-    Base::add_rpc_class<Timer>();
-    Base::add_rpc_class<BusOut>();
-    Base::add_rpc_class<BusIn>();
-    Base::add_rpc_class<BusInOut>();
-    Base::add_rpc_class<Serial>();
+  //Turn the LEDs off
+  led1.write(1);
+  led2.write(1);
+  led3.write(1);
+  led4.write(1);
+  
+  RPC::add_rpc_class<RpcDigitalOut>();
 
   printf("Setting up...\n");
-  EthernetErr ethErr = eth.setup();
-  if(ethErr)
+  eth.init();
+  int ethErr = eth.connect();
+  if(ethErr < 0)
   {
     printf("Error %d in setup.\n", ethErr);
     return -1;
   }
-  printf("Setup OK\n");
   
-  FSHandler::mount("/webfs", "/files"); //Mount /webfs path on /files web path
-  FSHandler::mount("/webfs", "/"); //Mount /webfs path on web root path
+  svr.addHandler<HTTPRpcRequestHandler>("/rpc");
   
-  svr.addHandler<SimpleHandler>("/hello");
-  svr.addHandler<RPCHandler>("/rpc");
-  svr.addHandler<FSHandler>("/files");
-  svr.addHandler<FSHandler>("/"); //Default handler
-  //Example : Access to mbed.htm : http://a.b.c.d/mbed.htm or http://a.b.c.d/files/mbed.htm
-  
-  svr.bind(80);
+  //attach server to port 80
+  svr.start(80, &eth);
   
   printf("Listening...\n");
     
@@ -73,14 +63,12 @@ int main() {
   //Listen indefinitely
   while(true)
   {
-    Net::poll();
+    svr.poll();
     if(tm.read()>.5)
     {
-      led1=!led1; //Show that we are alive
+      //led1=!led1; //Show that we are alive
       tm.start();
     }
   }
-  
-  return 0;
 
 }
